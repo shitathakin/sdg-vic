316dc07e95bfb767ece74ac4617395f83e125024
Jack Barrett <barrettjack7@gmail.com>
2020-10-31 16:05:58 +1100

    adding rest of website

---
+++ Documents/ISS project/newArchitecture/webpack.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+    entry: [
+      './src/index.js',
+      './src/styles.css'
+    ],
+    output: {
+      path: __dirname,
+      publicPath: '/',
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: "style-loader"
+            },
+            {
+              loader: "css-loader",
+              options: {
+                modules: true,
+                importLoaders: 1,
+                localIdentName: "[name]_[local]_[hash:base64]",
+                sourceMap: true,
+                minimize: true
+              }
+            }
+          ]
+        }
+      ]
+    }
+  };
